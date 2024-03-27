ce01806955091186a7ac39ea46d610b9d53eee8f
Yeseul Gong <0ysgong@gmail.com>
2024-03-27 19:15:01 +0900

    그래프의 표현

--- src/doit/graph/Main_1707_이분그래프판별하기.java
+++ src/doit/graph/Main_1707_이분그래프판별하기.java
@@ -1,4 +1,59 @@
 package doit.graph;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class Main_1707_이분그래프판별하기 {
+    static ArrayList<Integer>[] adj;
+    static boolean[] visited;
+    static int[] check;
+    static boolean isEven;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        for(int tc=0; tc<K; tc++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+
+            visited = new boolean[V+1];
+            check = new int[V+1];
+            adj = new ArrayList[V+1];
+            for(int i=1; i<=V; i++){
+                adj[i] = new ArrayList<>();
+            }
+
+            for(int i=0; i<E; i++){
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                adj[a].add(b);
+                adj[b].add(a);
+            }
+            isEven = true;
+            for(int i=1; i<=V; i++){
+                if(!isEven) break;
+                DFS(i);
+            }
+
+            if(isEven) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+
+    static void DFS(int now){
+        visited[now] = true;
+
+        for(int next: adj[now]){
+            if(!visited[next]) {
+                check[next] = (check[now]+1) % 2;
+                DFS(next);
+            } else if(check[next] == check[now]){
+                isEven = false;
+            }
+        }
+    }
 }
--- src/doit/graph/Main_2251_물의양구하기.java
+++ src/doit/graph/Main_2251_물의양구하기.java
@@ -1,4 +1,66 @@
 package doit.graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Main_2251_물의양구하기 {
+    static class AB {
+        int A, B; // A,B 물통의 양
+        public AB (int A, int B){
+            this.A = A;
+            this.B = B;
+        }
+    }
+    static int[] Sender = {0, 0, 1, 1, 2, 2};
+    static int[] Receiver = {1, 2, 0, 2, 0, 1};
+    static boolean visited[][];
+    static int now[];
+    static boolean answer[];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        now = new int[3]; // 주어진 물통의 양을 저장하는 배열
+        now[0] = sc.nextInt();
+        now[1] = sc.nextInt();
+        now[2] = sc.nextInt();
+
+        visited = new boolean[201][201]; // 방문한 조합을 체크함
+        answer = new boolean[201];
+
+        BFS();
+        for(int i=0; i<answer.length; i++){
+            if(answer[i]) System.out.print(i+" ");
+        }
+    }
+
+    static void BFS(){
+        Queue<AB> que = new LinkedList<>();
+        que.add(new AB(0,0));
+        visited[0][0] =true;
+        answer[now[2]] = true;
+
+        while(!que.isEmpty()){
+            AB p = que.poll();
+            int A = p.A;
+            int B = p.B;
+            int C = now[2] - A - B;
+
+            for(int k=0; k<6; k++){
+                int[] next = {A, B, C};
+                next[Receiver[k]] += next[Sender[k]];
+                next[Sender[k]] = 0;
+                if(next[Receiver[k]] > now[Receiver[k]]){ // 물이 넘칠 때
+                    next[Sender[k]] = next[Receiver[k]] - now[Receiver[k]]; // 초과하는 만큼 이전 물통에 넣어 줌
+                    next[Receiver[k]] = now[Receiver[k]]; // 대상 물통은 최대로 채워 줌
+                }
+                if(!visited[next[0]][next[1]]){
+                    visited[next[0]][next[1]] = true;
+                    que.add(new AB(next[0], next[1]));
+                    if(next[0] == 0){
+                        answer[next[2]] = true;
+                    }
+                }
+            }
+        }
+    }
 }
