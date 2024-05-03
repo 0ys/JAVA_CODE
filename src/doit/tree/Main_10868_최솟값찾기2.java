806de56c17af716cdb5104cf8603f96425b50b52
0ys <ysgong@yonsei.ac.kr>
2024-05-03 17:44:31 +0900

    인덱스 트리

--- src/doit/tree/Main_10868_최솟값찾기2.java
+++ src/doit/tree/Main_10868_최솟값찾기2.java
@@ -1,4 +1,79 @@
 package doit.tree;
 
+import java.io.*;
+import java.util.*;
+
 public class Main_10868_최솟값찾기2 {
+    static int N, M;
+    static int leafPointer;
+    static int[] indexTree;
+    static int[] inputs;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        inputs = new int[N];
+        for(int i = 0; i < N; i++) {
+            inputs[i] = Integer.parseInt(br.readLine());
+        }
+
+        initTree();
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write(Query(a, b)+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void initTree() {
+        int leaftCnt = 1;
+        while(leaftCnt < N){
+            leaftCnt *= 2;
+        }
+
+        indexTree = new int[leaftCnt*2];
+        Arrays.fill(indexTree, Integer.MAX_VALUE);
+        leafPointer = leaftCnt;
+
+        for(int i = 0; i < N; i++){
+            indexTree[leafPointer + i] = inputs[i];
+        }
+
+        for(int i = leafPointer-1; i > 0; i--){
+            indexTree[i] = Math.min(indexTree[i*2], indexTree[i*2+1]);
+        }
+    }
+
+    static int Query(int a, int b) {
+        int left = leafPointer + a - 1;
+        int right = leafPointer + b - 1;
+        int minValue = Integer.MAX_VALUE;
+
+        while(left <= right){
+            if(left % 2 == 1){
+                minValue = Math.min(minValue, indexTree[left]);
+                left++;
+            }
+            if(right % 2 == 0){
+                minValue = Math.min(minValue, indexTree[right]);
+                right--;
+            }
+
+            left /= 2;
+            right /= 2;
+        }
+
+        return minValue;
+    }
 }
--- src/doit/tree/Main_11505_구간곱구하기.java
+++ src/doit/tree/Main_11505_구간곱구하기.java
@@ -1,4 +1,90 @@
 package doit.tree;
 
+import java.io.*;
+import java.util.*;
+
 public class Main_11505_구간곱구하기 {
+    static int modNum = 1000000007; // 나머지로 나오는 결과를 잘 생각해야 함!
+    static int N, M, K;
+    static long[] indexTree;
+    static int leafPointer;
+    static long[] inputs;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        inputs = new long[N];
+        for(int i = 0; i < N; i++) {
+            inputs[i] = Long.parseLong(br.readLine());
+        }
+
+        initTree();
+
+        for(int i = 0; i < M+K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if(a == 1){
+                Update(b, c);
+            } else {
+                System.out.println(Query(b, c));
+            }
+        }
+    }
+
+    static void initTree(){
+        int leafCnt = 1;
+        while(leafCnt < N){
+            leafCnt *= 2;
+        }
+
+        leafPointer = leafCnt;
+        indexTree = new long[leafCnt*2];
+        Arrays.fill(indexTree, 1);
+
+        for(int i = 0; i < N; i++){
+            indexTree[leafPointer+i] = inputs[i];
+        }
+
+        for(int i = leafPointer-1; i > 0; i--){
+            indexTree[i] = (indexTree[i*2] * indexTree[i*2+1]) % modNum;
+        }
+    }
+
+    static void Update(int index, int value) {
+        index += leafPointer - 1;
+        indexTree[index] = value;
+
+        for(int i = index/2 ; i > 0; i /= 2){
+            indexTree[i] = (indexTree[i*2] * indexTree[i*2+1]) % modNum;
+        }
+    }
+
+    static long Query(int left, int right) {
+        left += leafPointer - 1;
+        right += leafPointer - 1;
+
+        long result = 1L;
+        while(left <= right){
+            if(left % 2 == 1){
+                result *= indexTree[left];
+                result %= modNum;
+                left++;
+            }
+            if(right % 2 == 0){
+                result *= indexTree[right];
+                result %= modNum;
+                right--;
+            }
+            left /= 2;
+            right /= 2;
+        }
+
+        return result;
+    }
 }
