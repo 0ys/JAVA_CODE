99db7d811c1d1805f7a636bde126b48054b6f6a4
0ys <ysgong@yonsei.ac.kr>
2024-05-16 19:16:14 +0900

    hashmap

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        HashMap<Integer, Integer> m = new HashMap<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        m.put(5, 6); // m.put(K, V);
+        m.put(2, 2);
+        m.put(10, 1);
+
+        if(m.containsKey(2)) System.out.println(m.get(2)); // m.get(K);
+
+        m.remove(5);
+
+        if(!m.containsKey(5)) System.out.println("not exists!");
+
+        m.put(2, 10); // key가 2인 곳의 value 값을 업데이트함
+        System.out.println(m.get(2)); // 10
+
+        //전체 출력 : entrySet() 활용
+        for (Map.Entry<Integer, Integer> entry: m.entrySet()) {
+            System.out.println("key: " + entry.getKey() + ", value: " + entry.getValue());
         }
     }
 }
